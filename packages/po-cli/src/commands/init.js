faa1d858912186358e9a9c85186fa28f61007c42
wangqinyong1994 <574812662@qq.com>
2021-04-07 15:55:19 +0800

    :pencil2: 添加cli打印

--- packages/po-cli/src/commands/init.js
+++ packages/po-cli/src/commands/init.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: ngwang
+ * @Date: 2021-02-08 15:07:59
+ * @LastEditors: ngwang
+ * @LastEditTime: 2021-04-07 15:54:43
+ */
 const process = require("process");
 const path = require("path");
 const fse = require("fs-extra");
@@ -10,6 +16,7 @@ module.exports = (options) => {
   inquirer.prompt([]);
   const destPath = path.resolve(__dirname, "../../temp");
   const spinner = ora("下载模版中...").start();
+  console.log("options:", options);
   // TODO: 选择不同模板
   download("github:wangqinyong1994/taro3-templates", destPath, (err) => {
     if (err) {
