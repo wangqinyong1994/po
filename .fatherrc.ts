372c3f78dde0afe710ef0aefca862715163a6eca
wangqinyong1994 <574812662@qq.com>
2021-04-07 15:52:32 +0800

    :tada: 新增eslint-config-po

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,12 @@
+/*
+ * @Author: ngwang
+ * @Date: 2021-02-07 09:33:31
+ * @LastEditors: ngwang
+ * @LastEditTime: 2021-04-07 15:48:51
+ */
 export default {
   target: "node",
   cjs: { type: "babel", lazy: true },
   disableTypeCheck: true,
-  pkgs: ["po-utils", "po-config"],
+  pkgs: ["po-utils", "po-config", "eslint-config-po", "po-cli"],
 };
