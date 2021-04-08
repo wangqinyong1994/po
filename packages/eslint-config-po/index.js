4c0fe51e7a8f0bfe329e87d5b4990a49e0f953fa
wangqinyong1994 <574812662@qq.com>
2021-04-08 10:44:58 +0800

    :wrench: 添加eslint-prettier

--- packages/eslint-config-po/index.js
+++ packages/eslint-config-po/index.js
@@ -2,11 +2,11 @@
  * @Author: ngwang
  * @Date: 2021-04-07 09:48:34
  * @LastEditors: ngwang
- * @LastEditTime: 2021-04-07 16:57:08
+ * @LastEditTime: 2021-04-08 10:43:32
  */
 module.exports = {
   root: true,
-  extends: ["airbnb"],
+  extends: ["airbnb", "prettier"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -19,6 +19,15 @@ module.exports = {
     // 通用配置override
     quotes: ["error", "single"],
     "arrow-parens": "off",
+    "object-curly-newline": [
+      "error",
+      {
+        ObjectExpression: "always",
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: "never",
+        ExportDeclaration: { multiline: true, minProperties: 3 },
+      },
+    ],
     // react插件override
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/react-in-jsx-scope": "off",
