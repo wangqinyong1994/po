8a17a87fa059a40ac3b11085de2d71c6ae1aeadf
wangqinyong1994 <574812662@qq.com>
2021-04-25 14:13:03 +0800

    :sparkles: 新增init模版命令

--- packages/po-cli/src/commands/init.js
+++ packages/po-cli/src/commands/init.js
@@ -1,27 +1,50 @@
-/*
- * @Author: ngwang
- * @Date: 2021-02-08 15:07:59
- * @LastEditors: ngwang
- * @LastEditTime: 2021-04-08 11:29:59
- */
 const process = require("process");
-const path = require("path");
-const fse = require("fs-extra");
+const { exit } = require("yargs");
 const ora = require("ora");
 const chalk = require("chalk");
 const download = require("download-git-repo");
 const inquirer = require("inquirer");
 
-module.exports = (options) => {
-  inquirer.prompt([]);
+module.exports = async (options) => {
   const destPath = process.cwd();
-  const spinner = ora("下载模版中...").start();
-  console.log("options:", options);
   // TODO: 选择不同模板
-  download("github:wangqinyong1994/taro3-templates", destPath, (err) => {
-    if (err) {
-      return spinner.fail(chalk.red("下载模版失败😈"));
+  // download("github:wangqinyong1994/taro3-templates", destPath, (err) => {
+  //   if (err) {
+  //     return spinner.fail(chalk.red("下载模版失败😈"));
+  //   }
+  //   spinner.succeed(chalk.green("下载模版成功🎆"));
+  // });
+
+  try {
+    const { tplName } = await inquirer.prompt([
+      {
+        name: "tplName",
+        type: "list",
+        message: `${chalk.yellow("想要获取哪个模板?")}`,
+        choices: ["honeycomb template"],
+      },
+    ]);
+    if (!tplName) {
+      console.log(`${chalk.red("未选择模版")}`);
+      exit(1);
+    } else {
+      if (tplName === "honeycomb template") {
+        const spinner = ora("下载模版中...").start();
+        download(
+          "direct:http://gitlab.iwhalecloud.com/pillar/whale-honeycomb.git#dev-template",
+          destPath,
+          { clone: true },
+          (err) => {
+            if (err) {
+              console.log(err);
+              return spinner.fail(chalk.red("下载模版失败😈"));
+            }
+            spinner.succeed(chalk.green("下载模版成功🎆"));
+          }
+        );
+      }
     }
-    spinner.succeed(chalk.green("下载模版成功🎆"));
-  });
+  } catch (error) {
+    console.log("error: ", error);
+  }
 };
